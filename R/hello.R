ecdcf82db1c1f8fbd4c32f37f6a2c3720a51e63e
leungarthur <46766354+leungarthur@users.noreply.github.com>
2024-10-29 13:01:11 -0400

    initialize

---
+++ R/hello.R
@@ -0,0 +1,6 @@
+# Functions for analysis of data generated from the Hewlett Packard 8452A spectrophotometer.
+
+# Read .WAV file for chlorophyll concentrations
+hello <- function() {
+  print("Hello, world!")
+}
